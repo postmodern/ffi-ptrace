2baa1a3e6ba1631dee778a9cb14b92baa9ebb24d
postmodern <postmodern.mod3@gmail.com>
2010-02-16 14:27:43 -0800

    Added the REGS enum based off of sys/reg.h.

--- lib/ptrace/types.rb
+++ lib/ptrace/types.rb
@@ -5,5 +5,57 @@ module FFI
     extend FFI::Library
 
     typedef :int, :pid_t
+
+    if Types::ULONG.size == 8
+      REGS = enum [
+        :r15,
+        :r14,
+        :r13,
+        :r12,
+        :rbp,
+        :rbx,
+        :r11,
+        :r10,
+        :r9,
+        :r8,
+        :rax,
+        :rcx,
+        :rdx,
+        :rsi,
+        :rdi,
+        :orig_rax,
+        :rip,
+        :cs,
+        :eflags,
+        :rsp,
+        :ss,
+        :fs_base,
+        :gs_base,
+        :ds,
+        :es,
+        :fs,
+        :gs
+      ]
+    else
+      REGS = enum [
+        :ebx,
+        :ecx,
+        :edx,
+        :esi,
+        :edi,
+        :ebp,
+        :eax,
+        :ds,
+        :es,
+        :fs,
+        :gs,
+        :orig_eax,
+        :eip,
+        :cs,
+        :efl,
+        :uesp,
+        :ss
+      ]
+    end
   end
 end
