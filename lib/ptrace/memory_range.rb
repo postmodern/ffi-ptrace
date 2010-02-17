bd1c9aa3c285a9a71765a5182618fa62b0901014
postmodern <postmodern.mod3@gmail.com>
2010-02-16 19:11:11 -0800

    Added MemorySpace and MemoryRange.

---
+++ lib/ptrace/memory_range.rb
@@ -0,0 +1,90 @@
+require 'ptrace/types'
+
+module FFI
+  module PTrace
+    class MemoryRange
+
+      include Enumerable
+
+      #
+      # Creates an `Enumerable` memory range.
+      #
+      # @param [MemorySpace] memory
+      #   The memory space that the range covers.
+      #
+      # @param [Integer] lower
+      #   The lower bound address of the range.
+      #
+      # @param [Integer] upper
+      #   The upper bound address of the range.
+      #
+      def initialize(memory,lower,upper)
+        @memory = memory
+        @lower = lower
+        @upper = upper
+      end
+
+      #
+      # Reads data from the memory range.
+      #
+      # @param [Range<Integer>, Integer] addr
+      #   The address(es) to read from.
+      #
+      # @return [MemoryRange, Integer]
+      #   If the given address was a Range, then a new MemoryRange will be returned.
+      #   If the given address was an Integer, then the word at the given address will be read.
+      #
+      def [](addr)
+        if addr.kind_of?(Range)
+          base_addr = @lower + addr.begin
+
+          self.class.new(@memory,base_addr,base_addr + addr.end)
+        else
+          @memory[@lower + addr]
+        end
+      end
+
+      #
+      # Writes data to the memory range.
+      #
+      # @param [Range<Integer>, Integer] addr
+      #   The address(es) to write to.
+      #
+      # @param [Range, Array<Integer>, Integer] data
+      #   The data to write.
+      #
+      def []=(addr,data)
+        if addr.kind_of?(Range)
+          base_addr = @lower + addr.begin
+
+          @memory[(base_addr..(base_addr + addr.end))] = data
+        else
+          @memory[@lower + addr] = data
+        end
+
+        return data
+      end
+
+      #
+      # Enumerates over the words within the memory range.
+      #
+      # @yield [word]
+      #   The given block will be passed each word in the memory range.
+      #
+      # @yieldparam [Integer] word
+      #   A word read from the memory range.
+      #
+      # @return [MemoryRange]
+      #   The memory range.
+      #
+      def each
+        (@lower..@upper).step(WORD_SIZE).each do |addr|
+          yield @memory[addr]
+        end
+
+        return self
+      end
+
+    end
+  end
+end
---
+++ lib/ptrace/memory_space.rb
@@ -0,0 +1,85 @@
+require 'ptrace/types'
+require 'ptrace/process'
+require 'ptrace/memory_range'
+
+module FFI
+  module PTrace
+    class MemorySpace
+
+      #
+      # Creates a new memory space.
+      #
+      # @param [Process] process
+      #   The process who's memory that will be accessed.
+      #
+      # @param [Symbol] read_method
+      #   The method to call from the process to read memory.
+      #
+      # @param [Symbol] write_method
+      #   The method to call from the process to write memory.
+      #
+      def initialize(process,read_method,write_method)
+        @process = process
+        @read_method = read_method
+        @write_method = write_method
+      end
+
+      #
+      # Reads memory from the process.
+      #
+      # @param [Range<Integer>, Integer] addr
+      #   The address(es) to read from.
+      #
+      # @param [Integer] length
+      #   The number of words to read.
+      #
+      # @return [MemoryRange, Array<Integer>, Integer]
+      #   If the address was a `Range` then a MemoryRange object will be returned.
+      #   If a length was given, an Array of words will be returned.
+      #   If the address was an Integer, a single word will be returned.
+      #
+      def [](addr,length=1)
+        if addr.kind_of?(Range)
+          MemoryRange.new(self,addr.begin,addr.end)
+        elsif length > 1
+          (0..length).step(WORD_SIZE).map do |index|
+            @process.send(@read_method,addr + index)
+          end
+        else
+          @process.send(@read_method,addr)
+        end
+      end
+
+      #
+      # Writes data to the memory of a process.
+      #
+      # @param [Range, Integer] addr
+      #   The address(es) to write data to.
+      #
+      # @param [Range, Array<Integer>, Integer] data
+      #   The data to write.
+      #
+      # @return [Range, Array<Integer>, Integer]
+      #   The written data.
+      #
+      def []=(addr,data)
+        if addr.kind_of?(Range)
+          if (data.kind_of?(Array) || data.kind_of?(Range))
+            data.each_with_index do |element,index|
+              @process.send(@write_method,addr.begin + (index * WORD_SIZE),element)
+            end
+          else
+            addr.step(WORD_SIZE).each do |index|
+              @process.send(@write_method,index,data)
+            end
+          end
+        else
+          @process.send(@write_method,addr,data)
+        end
+
+        return data
+      end
+
+    end
+  end
+end
