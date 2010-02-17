5a535a2eb80133a5a6c141a5e03e417646fbf5ce
postmodern <postmodern.mod3@gmail.com>
2010-02-16 18:53:21 -0800

    Added documentation to PTrace.allow!.

--- lib/ptrace/ptrace.rb
+++ lib/ptrace/ptrace.rb
@@ -2,6 +2,9 @@ require 'ptrace/ffi'
 
 module FFI
   module PTrace
+    #
+    # Makes the current process traceable by `ptrace`.
+    #
     def PTrace.allow!
       PTrace.ptrace(:ptrace_traceme, 0, nil, nil)
     end
