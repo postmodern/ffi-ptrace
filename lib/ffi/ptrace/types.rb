b3b152a0e5d78d9b2fc2acd4951c31e301f41e33
Postmodern <postmodern.mod3@gmail.com>
2011-05-24 20:26:18 -0700

    Removed the old REGS constant.

--- lib/ffi/ptrace/types.rb
+++ lib/ffi/ptrace/types.rb
@@ -12,58 +12,6 @@ module FFI
                    :big
                  end
 
-    if WORD_SIZE == 8
-      REGS = enum [
-        :r15,
-        :r14,
-        :r13,
-        :r12,
-        :rbp,
-        :rbx,
-        :r11,
-        :r10,
-        :r9,
-        :r8,
-        :rax,
-        :rcx,
-        :rdx,
-        :rsi,
-        :rdi,
-        :orig_rax,
-        :rip,
-        :cs,
-        :eflags,
-        :rsp,
-        :ss,
-        :fs_base,
-        :gs_base,
-        :ds,
-        :es,
-        :fs,
-        :gs
-      ]
-    else
-      REGS = enum [
-        :ebx,
-        :ecx,
-        :edx,
-        :esi,
-        :edi,
-        :ebp,
-        :eax,
-        :ds,
-        :es,
-        :fs,
-        :gs,
-        :orig_eax,
-        :eip,
-        :cs,
-        :efl,
-        :uesp,
-        :ss
-      ]
-    end
-
     # Operation not permitted
     EPERM = 1
 
