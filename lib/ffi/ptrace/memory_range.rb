aecea66cf26f70bf602c0ee3c5caa4b848a08594
Postmodern <postmodern.mod3@gmail.com>
2011-05-24 17:32:31 -0700

    Updated MemoryRange to use more of MemorySpace.

--- lib/ffi/ptrace/memory_range.rb
+++ lib/ffi/ptrace/memory_range.rb
@@ -31,38 +31,32 @@ module FFI
       #   The address(es) to read from.
       #
       # @return [MemoryRange, Integer]
-      #   If the given address was a Range, then a new MemoryRange will be returned.
+      #   If the given address was a Range, then a new {MemoryRange} will be returned.
       #   If the given address was an Integer, then the word at the given address will be read.
       #
-      def [](addr)
-        if addr.kind_of?(Range)
-          base_addr = @lower + addr.begin
+      def [](index,length=1)
+        if index.kind_of?(Range)
+          base = @lower + index.begin
 
-          self.class.new(@memory,base_addr,base_addr + addr.end)
+          @memory[Range.new(base,base+index.end)]
+        elsif length > 1
+          @memory[@lower + index, length]
         else
-          @memory[@lower + addr]
+          @memory[@lower + index]
         end
       end
 
       #
       # Writes data to the memory range.
       #
-      # @param [Range<Integer>, Integer] addr
+      # @param [Integer] addr
       #   The address(es) to write to.
       #
-      # @param [Range, Array<Integer>, Integer] data
+      # @param [Array<Integer>, String, Integer] data
       #   The data to write.
       #
       def []=(addr,data)
-        if addr.kind_of?(Range)
-          base_addr = @lower + addr.begin
-
-          @memory[(base_addr..(base_addr + addr.end))] = data
-        else
-          @memory[@lower + addr] = data
-        end
-
-        return data
+        @memory[addr] = data
       end
 
       #
