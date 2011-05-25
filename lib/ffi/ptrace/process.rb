099a6ad65455bb9eda64bb8f1f33bb90740ed673
Postmodern <postmodern.mod3@gmail.com>
2011-05-24 19:58:30 -0700

    Only check PTrace.errno if ptrace returned -1.

--- lib/ffi/ptrace/process.rb
+++ lib/ffi/ptrace/process.rb
@@ -283,13 +283,15 @@ module FFI
       def ptrace(request,addr=nil,data=nil)
         ret = PTrace.ptrace(request,@pid,addr,data)
 
-        case PTrace.errno
-        when PTrace::EPERM
-          raise(Errno::EACCESS,"The requested process (#{@pid}) couldn't be traced. Permission denied")
-        when PTrace::ESRCH
-          raise(RuntimeError,"The requested process (#{@pid}) doesn't exist, not being traced or not stopped")
-        when PTrace::EIO
-          raise(IOError,"The ptrace request was invalid or read/write was made from/to invalid area of memory")
+        if ret == -1
+          case PTrace.errno
+          when PTrace::EPERM
+            raise(Errno::EACCESS,"The requested process (#{@pid}) couldn't be traced. Permission denied")
+          when PTrace::ESRCH
+            raise(RuntimeError,"The requested process (#{@pid}) doesn't exist, not being traced or not stopped")
+          when PTrace::EIO
+            raise(IOError,"The ptrace request was invalid or read/write was made from/to invalid area of memory")
+          end
         end
 
         return ret
