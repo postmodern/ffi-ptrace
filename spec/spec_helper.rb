52b687bfb61fdc7d6714774d2ccc3c901e37c489
Postmodern <postmodern.mod3@gmail.com>
2012-04-23 00:41:39 -0700

    Compile the program.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,13 @@ require 'ffi/ptrace/version'
 
 include FFI
 include FFI::PTrace
+
+CC             = (ENV['CC'] || 'cc')
+PROGRAM        = File.join('spec','program')
+PROGRAM_SOURCE = File.join('spec','program.c')
+
+RSpec.configure do |spec|
+  spec.before(:suite) do
+    system CC, '-m32', PROGRAM_SOURCE, '-o', PROGRAM
+  end
+end
